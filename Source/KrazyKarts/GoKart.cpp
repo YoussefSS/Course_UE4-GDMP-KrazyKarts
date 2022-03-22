548001544d05f6d5caee6c350528c9d70b9ca9ed
YoussefSS <joesamir98@gmail.com>
2022-03-22 12:32:20 +0200

    79. Creating A Go-Kart Pawn

---
+++ Source/KrazyKarts/GoKart.cpp
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GoKart.h"
+#include "Components\InputComponent.h"
+
+// Sets default values
+AGoKart::AGoKart()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGoKart::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGoKart::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector Translation = Velocity * DeltaTime * 100; // Multiplying by deltatime makes it just meters now, multiplying by 100 makes it centimeters. So now we are adding this movement per tick (20 meters per second)
+	AddActorWorldOffset(Translation);
+}
+
+// Called to bind functionality to input
+void AGoKart::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &AGoKart::MoveForward);
+
+}
+
+void AGoKart::MoveForward(float Value)
+{
+	Velocity = GetActorForwardVector() * 20 * Value; // Meters per second
+}
+
